a8b4df2ad37e382b30b78ad84d3b2ebf0d009ec2
Zlatko Matokanovic <zmatokan@gmail.com>
2018-04-09 15:14:48 +0200

    test
    test

--- BullsEye/AppDelegate.swift
+++ BullsEye/AppDelegate.swift
@@ -28,6 +28,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
   var window: UIWindow?
 
 
+    print();
   func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
     // Override point for customization after application launch.
     return true
